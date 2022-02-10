1106291ec14ffa634dab9c176265e0f386b0cdbe
SchawnnDev <iperskill@gmail.com>
2022-02-10 15:19:17 +0100

    ui loaded

--- src/Widgets/mainwindow.cpp
+++ src/Widgets/mainwindow.cpp
@@ -1,10 +1,12 @@
 #include "ui_mainwindow.h"
-#include "../../include/Widgets/mainwindow.h"
+#include "include/Widgets/mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
 	:QMainWindow(parent),
 	ui(new Ui::MainWindow)
-{}
+{
+	ui->setupUi(this);
+}
 
 MainWindow::~MainWindow()
 {
